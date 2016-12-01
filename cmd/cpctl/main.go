0e5d9f11024411bbdcfdd508902ae7d694549837
Denis Lavrov <bahus.vel@gmail.com>
2016-12-02 00:56:56 +1300

    Added node status printing

--- cmd/cpctl/main.go
+++ cmd/cpctl/main.go
@@ -2,9 +2,42 @@ package main
 
 import (
 	"os"
+	"reflect"
 
+	"github.com/bahusvel/ClusterPipe/common"
+	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
 )
+
+var clusterNodes []common.CPD
+
+func fieldNames(value interface{}) []string {
+	v := reflect.TypeOf(value)
+	fieldNames := []string{}
+	for i := 0; i < v.NumField(); i++ {
+		fieldNames = append(fieldNames, v.Field(i).Name)
+	}
+	return fieldNames
+}
+
+func fieldValues(value interface{}) []string {
+	v := reflect.ValueOf(value)
+	fieldValues := []string{}
+	for i := 0; i < v.NumField(); i++ {
+		fieldValues = append(fieldValues, v.Field(i).String())
+	}
+	return fieldValues
+}
+
+func NodeStatus(c *cli.Context) error {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(fieldNames(common.CPD{}))
+	for _, node := range clusterNodes {
+		table.Append(fieldValues(node))
+	}
+	table.Render()
+	return nil
+}
 
 func main() {
 	app := cli.NewApp()
@@ -12,6 +45,10 @@ func main() {
 	app.Commands = []cli.Command{
 		{
 			Name: "ps",
+		},
+		{
+			Name:   "nodes",
+			Action: NodeStatus,
 		},
 		{
 			Name: "start",
